98e2eb3dd3a7c050004252e187df13179558901a
Watson <watson1978@gmail.com>
2019-02-02 14:12:46 +0900

    add

---
+++ linux-setup.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+mkdir ~/bin
+wget https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_amd64.tar.gz
+tar xf peco_linux_amd64.tar.gz
+cp peco_linux_amd64/peco ~/bin
+
+# oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# anyframe
+if [ ! -d ~/.zsh/anyframe ]; then
+	git clone git@github.com:mollifier/anyframe.git ~/.zsh/anyframe
+fi
+
+# rbenv
+if [ ! -d ~/.rbenv ]; then
+	git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+	git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+	git clone https://github.com/rkh/rbenv-update.git "$(rbenv root)"/plugins/rbenv-update
+fi
+
+# pyenv
+if [ ! -d ~/.pyenv ]; then
+	curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+fi
+
+sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+sudo apt-get update
+sudo apt-get install rcm
+
+env RCRC=$HOME/dotfiles/rcrc rcup
