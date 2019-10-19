23976675a2d4abd4810969b0a278a982a717065b
Watson <watson1978@gmail.com>
2019-10-19 18:26:46 +0900

    add apps

--- macos-setup.sh
+++ macos-setup.sh
@@ -41,6 +41,8 @@ mas install 1024640650 # CotEditor
 mas install 419330170 # Moom
 mas install 445189367 # PopClip
 mas install 425955336 # Skitch
+mas install 1081413713 # GIF Brewery 3
+mas install 578878161 # Sleep No More
 
 # oh-my-zsh
 if [ ! -d ~/.oh-my-zsh ]; then
