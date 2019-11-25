8f0c1433dfbfbda06279bbe2224ee48bbb405031
Watson <watson1978@gmail.com>
2019-11-25 23:48:55 +0900

    setup Homebrew at first

--- macos-setup.sh
+++ macos-setup.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+if !(type "brew" > /dev/null 2>&1); then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 brew install git
 brew install peco
 brew install chisel
